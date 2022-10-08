3bc653e6c0095b977e7e20a6f9754f6ec39bd6b2
semperubique <lastmurname@gmail.com>
2022-10-08 23:28:47 +0200

    Update to display

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const key = "b902e8317058b2cac1e5f3c013a5fb95";
-const location = "London";
 
 const processData = (weatherData) => {
   const weatherDataProcessed = {
@@ -22,16 +21,49 @@ const getData = async (key, location) => {
   try {
     const url = `http://api.openweathermap.org/data/2.5/forecast?q=${location}&APPID=${key}&units=metric`;
     const response = await fetch(url, { mode: "cors" });
-    const weatherData = await response.json();
-    const weatherDataProcessed = processData(weatherData);
-    return weatherDataProcessed;
+    if (response.status === 200) {
+      const weatherData = await response.json();
+      const weatherDataProcessed = processData(weatherData);
+      return weatherDataProcessed;
+    } else {
+      console.log(`Error: ${response.status}`);
+    }
   } catch (error) {
     console.log(error);
   }
 };
 
+const form = document.querySelector("form");
 const searchButton = document.querySelector(".search-button");
 searchButton.addEventListener("click", async () => {
-  const abc = await getData(key, location);
-  console.log(abc);
+  if (!form.checkValidity()) {
+    form.reportValidity();
+  } else {
+    const location = document.querySelector("input#location").value;
+    const cityName = document.querySelector(".city-name");
+    const weatherDescription = document.querySelector(".weather-description");
+    const currentTemp = document.querySelector(".current-temp");
+    const humidity = document.querySelector(".humidity");
+    const windSpeed = document.querySelector(".wind-speed");
+    document.querySelector("#loading").classList.remove("hidden");
+    document.querySelector(".today").classList.add("hidden");
+    const weatherData = await getData(key, location);
+    if (weatherData !== undefined) {
+      cityName.textContent = weatherData.name;
+      weatherDescription.textContent = weatherData.today.weatherDescription;
+      currentTemp.textContent = weatherData.today.currentTemp + "	°C";
+      humidity.textContent = "Humidity: " + weatherData.today.humidity + " %";
+      windSpeed.textContent =
+        "Wind speed: " + weatherData.today.windSpeed + " km/h";
+    } else {
+      document
+        .querySelector("input#location")
+        .setCustomValidity("Incorrect place name");
+      form.reportValidity();
+      document.querySelector("input#location").setCustomValidity("");
+      form.reset();
+    }
+    document.querySelector("#loading").classList.add("hidden");
+    document.querySelector(".today").classList.remove("hidden");
+  }
 });
